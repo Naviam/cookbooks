292f9051f6860d02b55b11521a1f1cb11f5a81e9
lkurts <lkurts@gmail.com>
2014-09-11 00:20:41 -0400

    initial bower recipe and kitchen/vagrant test for it

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-cookbooks
-=========
+PerksLab Cookbooks
+==================
+
+PerksLab Chef Cookbooks will be stored here.
---
+++ bower/metadata.rb
@@ -0,0 +1,6 @@
+name        'bower'
+version     '1.0.0'
+description 'Installs Bower'
+maintainer  'TrueCron Team'
+
+depends     'nodejs', '~> 2.1.0'
