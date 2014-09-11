06f08ec4194e4bc528e848f00e2887ec416d815b
lkurts <lkurts@gmail.com>
2014-09-11 00:54:10 -0400

    added switch to truecron folder

--- bower/recipes/default.rb
+++ bower/recipes/default.rb
@@ -1,7 +1,7 @@
 include_recipe "nodejs::nodejs_from_package"
 
 execute "npm-install-bower" do
-    #  cd "/var/www/truecron"
+    cd "/var/www/truecron"
     command "npm install -g bower"
     action :run
 end
